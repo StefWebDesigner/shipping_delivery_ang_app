45d7d5c476fcfcfa8c7325097272b53882c6458b
Stefan <stefan_riley@icloud.com>
2025-03-21 15:04:17 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'shipping_delivery_ang_app';
+}
