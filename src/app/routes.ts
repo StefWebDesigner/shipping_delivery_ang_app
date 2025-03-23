866b0fa7c954e3ad410b4724077888388d373e0b
Stefan <stefan_riley@icloud.com>
2025-03-23 10:20:06 -0400

    fixed the routing issue

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { HomeComponent } from "./components/home/home.component";
 import { DetailsComponent } from "./components/details/details.component";
+import { Routes } from "@angular/router";
 
 const routeConfig: Routes = [
     {
