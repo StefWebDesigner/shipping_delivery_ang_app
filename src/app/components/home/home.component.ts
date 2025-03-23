bf22f2ab571e7daaad683d3115c0dc0593d509b6
Stefan <stefan_riley@icloud.com>
2025-03-23 11:02:20 -0400

    added bootstrap & navbar

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { NavComponent } from "../nav/nav.component";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [NavComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
